49757307bf03305553dccc529373ad4db2e3b1f5
Omni <geoffrey.frioli@gmail.com>
2020-01-20 16:20:42 +0100

    style

--- resources/views/public/home.blade.php
+++ resources/views/public/home.blade.php
@@ -2,10 +2,21 @@
 
 @section('content')
 
-<div class="container-fluid accueil">
+<img src="/images/snack-2635035_1920.jpg" class="img-fluid" alt="Responsive image">
+<div class="container-fluid accueil text-white">
+  <div class="row">
+    <div class="col-7 offset-5 mt-5 pt-5 text-center">
+    <h1 class="Iamarock">Ouvert tous les jours<br>Sur place ou à empoter</h1>
+    <a type="button" class="btn mt-4 btnMenu" href="{{ route('public_menu') }}">Voir le menu</a>
+    </div>
+  </div>
+</div>
+
+<div class="container-fluid test">
 
 </div>
-<div class="container-fluid test">
+
+<div class="container-fluid test1">
 
 </div>
 
