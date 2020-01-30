56f8a30476fdda29937f8231e69080a19cf050af
Omni <geoffrey.frioli@gmail.com>
2020-01-30 17:36:34 +0100

    add iframe map

--- resources/views/contact/create.blade.php
+++ resources/views/contact/create.blade.php
@@ -65,5 +65,9 @@
     
   </div>
 </div>
+<div class="Redhr">
+
+</div>
+<iframe class="plan" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1023.5235622843728!2d-1.5666715934842528!3d43.482267443821954!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd516b2f7a3753f3%3A0xc16312733970622d!2sMoony!5e0!3m2!1sfr!2sfr!4v1580401147243!5m2!1sfr!2sfr" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
 
 @endsection
