99707c77654611afde23a4f84efa65e10c6a173b
Omni <geoffrey.frioli@gmail.com>
2020-01-31 11:12:01 +0100

    text contact thanks

--- resources/views/contact/thanks.blade.php
+++ resources/views/contact/thanks.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 
 <div class="container-fluid contactThanks d-flex align-items-center justify-content-center">
-		<div class="container text-center text-white">
-		 	Merci pour votre message, nous vous répondrons dans les meilleurs délais
+		<div class="container text-center">
+		 	<p class="contactTitle textShad Iamarock text-white">Merci pour votre message, nous vous répondrons dans les meilleurs délais</p>
 		</div>
 </div>
 
