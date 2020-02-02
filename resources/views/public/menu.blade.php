3083499b4532572843668fc6df6b0cce8ba8c267
Omni <geoffrey.frioli@gmail.com>
2020-02-02 18:37:22 +0100

    hello ok

--- resources/views/public/menu.blade.php
+++ resources/views/public/menu.blade.php
@@ -2,14 +2,14 @@
 
 @section('content')
 
-<div class="text-center pt-5 pb-5">
-<h1 class="text-center Iamarock mb-4 text-white">Le Menu</h1>
-  <img src="/images/rocknburger_menu_01.jpg" class="img-fluid menuImage mb-5" alt="">
-  <img src="/images/rocknburger_menu_02.jpg" class="img-fluid menuImage mb-5" alt="">
-  <img src="/images/rocknburger_desserts_01.jpg" class="img-fluid menuImage mb-5" alt="">
-<h2 class="text-center Iamarock mb-4 text-white">A Emporter</h2>
-  <img src="/images/rocknburger_panneau_01.jpg" class="img-fluid menuImage" alt="">
-  <img src="/images/rocknburger_panneau_02.jpg" class="img-fluid menuImage" alt="">
+<div class="text-center pt-5 pb-5 bg-white">
+<h1 class="text-center Iamarock mb-4 redCustom">Le Menu</h1>
+  <img src="/images/rocknburger_menu_01.jpg" class="img-fluid menuImage mb-5 carteMenu" alt="">
+  <img src="/images/rocknburger_menu_02.jpg" class="img-fluid menuImage mb-5 carteMenu" alt="">
+  <img src="/images/rocknburger_desserts_01.jpg" class="img-fluid menuImage mb-5 carteMenu" alt="">
+<h2 class="text-center Iamarock mb-4 redCustom">A Emporter</h2>
+  <img src="/images/rocknburger_panneau_01.jpg" class="img-fluid menuImage mb-5 carteMenu" alt="">
+  <img src="/images/rocknburger_panneau_02.jpg" class="img-fluid menuImage carteMenu" alt="">
 </div>
 
 @endsection
